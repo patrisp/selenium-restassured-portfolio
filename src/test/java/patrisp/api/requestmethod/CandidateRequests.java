2d1e7a4489b3d7c16c4cfcf34e457cfe83b79aeb
Patrycja Pawlowska <patriss93@gmail.com>
2025-09-14 19:43:43 +0200

    Fail interview method added

--- src/test/java/patrisp/api/requestmethod/CandidateRequests.java
+++ src/test/java/patrisp/api/requestmethod/CandidateRequests.java
@@ -3,6 +3,7 @@ package patrisp.api.requestmethod;
 import io.restassured.response.Response;
 import patrisp.api.Specifications;
 import patrisp.api.requestbody.CandidateDetails;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 
@@ -19,27 +20,30 @@ public class CandidateRequests extends Specifications {
         Response response = given()
                 .when()
                 .put("/api/v2/recruitment/candidates/" + candidateId + "/shortlist");
-        response.then().log().all();
-
     }
 
-    public void scheduleInterview(int candidateId) {
+    public void scheduleInterview(int candidateId, Map body) {
         Response response = given()
-                .body("{interviewName\":\"Interview\",\"interviewDate\":\"2025-08-18\",\"interviewTime\":null,\"note\":null,\"interviewerEmpNumbers\":[7]}\n")
+                .body(body)
                 .when()
                 .post("/api/v2/recruitment/candidates/" + candidateId + "/shedule-interview");
     }
 
+    public void failInterview(int candidateId, int interviewerId) {
+        Response response = given()
+                .when()
+                .put("/api/v2/recruitment/candidates/" + candidateId + "/interviews/" + interviewerId + "/fail");
+    }
+
+
     public void passInterview(int candidateId, int interviewerId ) {
         Response response = given()
-                .body("{\"note\":null}")
                 .when()
                 .put("/api/v2/recruitment/candidates/" + candidateId + "/interviews/" + interviewerId + "/pass");
     }
 
     public void offerJob(int candidateId) {
         Response response = given()
-                .body("{\"note\":null}")
                 .when()
                 .put("/api/v2/recruitment/candidates/" + candidateId + "/job/offer");
     }
