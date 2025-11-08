e1dd536fc84374b4bc267427231b37b69b36eecc
Patrycja Pawlowska <patriss93@gmail.com>
2025-11-08 18:32:18 +0100

    Update class to use Lombok builder

--- src/test/java/patrisp/testdata/EmployeeData.java
+++ src/test/java/patrisp/testdata/EmployeeData.java
@@ -1,42 +1,24 @@
 package patrisp.testdata;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class EmployeeData {
-    public String firstName;
-    public String middleName;
-    public String lastName;
-    public String id;
-
-    public EmployeeData() {
-        this.firstName = "Mary";
-        this.middleName = "Kate";
-        this.lastName = "Hopkins";
-        this.id = null;
-    }
-
-    public EmployeeData(String firstName, String middleName, String lastName, String id) {
-        this.firstName = firstName;
-        this.middleName = middleName;
-        this.lastName = lastName;
-        this.id = id;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setMiddleName(String middleName) {
-        this.middleName = middleName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String employeeId;
+    private String otherId;
+    private String driverLicenseNumber;
+    private String driverLicenseExpiryDate;
+    private String nationality;
+    private String maritalStatus;
+    private String dateOfBirth;
+    private String gender;
 }
